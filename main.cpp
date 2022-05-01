7bc5780a949af2d1e09720d1812fc01c968f4ca8
AnshSingh04 <73479734+AnshSingh04@users.noreply.github.com>
2022-05-01 15:29:08 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
-#include <iostream>
 #include "Individual.h"
-#include "Mutator.h"
-#include "BitFlip.h"
-#include "BitFlipProb.h"
-#include "Rearrange.h"
-using namespace std;
-Individual * execute(Individual * indPtr, Mutator * mPtr, int k);
+#include"Mutator.h"
+#include"BitFlip.h"
+#include <iostream>
+#include <string>
+#include"BitFlipProb.h"
+#include"Rearrange.h"
 
-Individual * execute(Individual * indPtr, Mutator * mPtr, int k) {
-    return indPtr;
-    //mPtr->mutate(&indPtr,k);
+using namespace std;
+Individual * execute(Individual * indPtr, Mutator * mPtr, int k){   
+    Individual *p;
+    p = new Individual(1);
+    p = mPtr->mutate(indPtr,k);
+    return p;
 }
 
-int main() {
-    string binarystr1="", binarystr2="";
-    int k1=0,k2=0;
-    cin>>binarystr1>>k1>>binarystr2>>k2;
-
-    Individual person1(binarystr1);
-    Individual person2(binarystr2);
-    BitFlip j;
+int main(void){
+    string binarystr1;
+    int k1;
+    string binarystr2;
+    int k2;
+    cin >> binarystr1 >> k1 >> binarystr2 >> k2;
+    Individual *person1;
+    person1 = new Individual(binarystr1);
+    Individual *person2;
+    person2 = new Individual(binarystr2);
     Rearrange r;
-
-    person1 = j.mutate(person1,k1);
-    person2 = r.mutate(person2,k2);
-    int ones = person2.getMaxOnes();
-    cout<<person1.getString()<<" "<<person2.getString()<<" "<<ones;
-    return 0;
+    BitFlip b;
+    person1 = execute(person1,&b,k1);
+    person2 = execute(person2,&r,k2);
+    std::cout << person1->getString() << " " << person2->getString() << " " << person2->getMaxOnes()<<std::endl;
 }
